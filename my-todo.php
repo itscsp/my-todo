67a55ac4878f1cd97ced221ce3245bc96663a5fb
itscsp <chethanspoojary1@gmail.com>
2025-05-29 22:11:02 +0530

    Deploy issue fixing and version updates

--- my-todo.php
+++ my-todo.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: My Todo
  * Description: A comprehensive todo management plugin
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Chethan S Poojary
  * Text Domain: my-todo
  */
